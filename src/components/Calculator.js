442f21a8610a7d435475ac417e8d0cfb2b0be26a
Ghilain <ishimweghilain5050@gmail.com>
2022-06-02 09:00:22 +0200

    Add css style and check linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,36 +12,37 @@ class Calculator extends React.Component {
     return (
       <div className="calcContainer">
         <div className="inputCalc">0</div>
-        <div className="calcNumber">
+        <div className="buttonContainer">
+          <div className="calcNumber">
+            <div className="calcNumber1">
+              <button type="button" className="ac itemcalc heightbtn">AC</button>
+              <button type="button" className="addneg itemcalc heightbtn">+/-</button>
+              <button type="button" className="percent itemcalc heightbtn">%</button>
+              <button type="button" className="seven itemcalc heightbtn">7</button>
+              <button type="button" className="eight itemcalc heightbtn">8</button>
+              <button type="button" className="nine itemcalc heightbtn">9</button>
+              <button type="button" className="four itemcalc heightbtn">4</button>
+              <button type="button" className="five itemcalc heightbtn">5</button>
+              <button type="button" className="six itemcalc heightbtn">6</button>
+              <button type="button" className="one itemcalc heightbtn">1</button>
+              <button type="button" className="two itemcalc heightbtn">2</button>
+              <button type="button" className="three itemcalc heightbtn">3</button>
+            </div>
 
-          <div className="calcNumber1">
-            <button type="button" className="ac itemcalc">AC</button>
-            <button type="button" className="addneg itemcalc">+/-</button>
-            <button type="button" className="percent itemcalc">%</button>
-            <button type="button" className="seven itemcalc">7</button>
-            <button type="button" className="eight itemcalc">8</button>
-            <button type="button" className="nine itemcalc">9</button>
-            <button type="button" className="four itemcalc">4</button>
-            <button type="button" className="five itemcalc">5</button>
-            <button type="button" className="six itemcalc">6</button>
-            <button type="button" className="one itemcalc">1</button>
-            <button type="button" className="two itemcalc">2</button>
-            <button type="button" className="three itemcalc">3</button>
+            <div className="calcNumber2">
+              <button type="button" className="zero itemcalc1 heightbtn">0</button>
+              <button type="button" className="dot itemcalc1 heightbtn">.</button>
+            </div>
+
           </div>
 
-          <div className="calcNumber2">
-            <button type="button" className="zero itemcalc1">0</button>
-            <button type="button" className="dot itemcalc1">.</button>
+          <div className="calcOperators">
+            <button type="button" className="divid">÷</button>
+            <button type="button" className="times">&times;</button>
+            <button type="button" className="minus">-</button>
+            <button type="button" className="add">+</button>
+            <button type="button" className="equal">=</button>
           </div>
-
-        </div>
-
-        <div className="calcOperators">
-          <button type="button" className="divid">÷</button>
-          <button type="button" className="times">&times;</button>
-          <button type="button" className="minus">-</button>
-          <button type="button" className="add">+</button>
-          <button type="button" className="equal">=</button>
         </div>
       </div>
     );
